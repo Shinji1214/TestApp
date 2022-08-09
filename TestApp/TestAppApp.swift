1058e215a8debbd11ee2552fc8bfef014fbdeb47
Chang <s.yoshida1214@outlook.jp>
2022-08-09 21:30:30 +0900

    Initial Commit

---
+++ TestApp/TestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAppApp.swift
+//  TestApp
+//
+//  Created by Yoshida Shinji on 2022/08/09.
+//
+
+import SwiftUI
+
+@main
+struct TestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
